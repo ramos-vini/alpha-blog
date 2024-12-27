55dff13be1426f1cd0aebef9c6c6c79cdddb499c
ramos-vini <ramoscontrole@gmail.com>
2024-12-27 18:22:44 -0300

    added validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  validates :username, presence: true, length: {minimum: 3, maximum: 20}
+  validates :email, presence: true, length: {maximum: 100}, email: true
 end
