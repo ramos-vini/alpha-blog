4b37ca6dd460449e4d1901f4b0449943d108df11
ramos-vini <ramoscontrole@gmail.com>
2025-01-15 19:13:15 -0300

    create login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   root "pages#home"
 
-  get "about", to: "pages#about"
-
   resources :articles
 
-  get 'signup', to: "users#new"
+  get "signup", to: "users#new"
 
   resources :users, except: [:new]
+
+  get "login", to: "session#new"
+  post "login", to: "session#create"
+  delete "login", to: "session#destroy"
 end
