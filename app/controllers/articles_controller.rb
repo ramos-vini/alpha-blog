04ac51a09c1c14b95f99884cc8e9454feeb2c70c
ramos-vini <ramoscontrole@gmail.com>
2025-01-14 22:06:56 -0300

    Add edit profile button to user profile page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(set_article_params)
+    # User ID is statically set to 1
     @article.user_id = 1
     if @article.save
       flash[:notice] = "Article was created successfully."
