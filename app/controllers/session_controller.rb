18df3e74aede87abe17b23b3de679f15c78f0c49
ramos-vini <ramoscontrole@gmail.com>
2025-01-15 20:08:12 -0300

    create session login and logout

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -3,8 +3,21 @@ class SessionController < ApplicationController
   end
 
   def create
+    @user = User.find_by(email: params[:session][:email])
+    
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      flash[:notice] = "You have successfully logged in."
+      redirect_to @user
+    else
+      flash.now[:alert] = "Email and Password do not match."
+      render 'new'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:alert] = "You have logged out."
+    redirect_to root_path
   end
 end
